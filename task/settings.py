485f58411f7547e73125d5d9b8dd1054e925f629
SRISAKTHIYA <srisakthiyaganesan@gmail.com>
2024-05-01 15:09:25 +0530

    changes

--- task/settings.py
+++ task/settings.py
@@ -122,7 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[(os.path.join(BASE_DIR, 'templates')),]
+#STATICFILES_DIRS=[(os.path.join(BASE_DIR, 'templates')),]
+STATIC_ROOT = BASE_DIR
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 
